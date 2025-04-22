0726ec7c28e595a9426639d85814905ef07a9f97
svinciak <kerata.marek1@gmail.com>
2025-04-22 10:15:10 +0200

    fixed windows

--- DungeonCrawler.py
+++ DungeonCrawler.py
@@ -65,9 +65,8 @@ def startGame(menuWin):
     PlaySound('gameStart.wav', 1)
     menuWin.after(1000)
     menuWin.destroy()
-    #createMap()
-    createGame()  
-    game.mainloop()
+    createMap()
+    #createGame()  
 
 def showScores():
     PlaySound('buttonClick.wav', 1)
@@ -95,15 +94,21 @@ def createMap():
     menuCanvas = tk.Canvas(levelWin, bg='red', width=500, height=500)
     menuCanvas.pack()
     
-    level1 = tk.Button(levelWin, text='Level 1', command=lambda win=levelWin: level1(win))
-    menuCanvas.create_window(250, 250, window=level1)
-
-    levelWin.mainloop()
+    level1Button = tk.Button(levelWin, text='Level 1', command=lambda: startLevel(levelWin, 1))
+    menuCanvas.create_window(250, 150, window=level1Button)
+    
+    level1Button = tk.Button(levelWin, text='Level 2', command=lambda: startLevel(levelWin, 2))
+    menuCanvas.create_window(250, 250, window=level1Button)
+    
+    level1Button = tk.Button(levelWin, text='Level 3', command=lambda: startLevel(levelWin, 3))
+    menuCanvas.create_window(250, 350, window=level1Button)
     
     # nastavit cez return ako cislo mapy, a potom vytvorit game
-
-def level1(levelWin):
+    
+def startLevel(levelWin, level):
     levelWin.destroy()
+    createGame(level)
+
 
 #-----#
 # HRA #
@@ -116,7 +121,7 @@ def level1(levelWin):
 
 dungeonLevel = 1 
 playerHealth = 100
-enemyList = []
+enemyCount = 1
 frameImgs = []
 usedWords = []
 letters = []
@@ -359,9 +364,11 @@ def isValidWord(word):
 def draw():
     pass
     
-def createEnemies(enemyType):
-    global enemy, animationID, usedWords
-    usedWords = []
+def createEnemies(level, enemyType):
+    global enemy, animationID
+    
+    if enemyCount == 0:    
+        return 
     
     if animationID:
         enemyCanvas.after_cancel(animationID)
@@ -369,7 +376,7 @@ def createEnemies(enemyType):
     
     enemy = {
         "type": enemyType,
-        "hp": 100,
+        "hp": 50+(50*level),
         "sound": "este neni, napotom",
         "id": enemyCanvas.create_image(150, 150, image=batImgs[0], tags="enemy")
     }
@@ -395,7 +402,7 @@ def damageEnemy(word):
     currentEnemy["hp"]-=dmg
     print(currentEnemy["hp"])
     updateEnemyHeatlh(currentEnemy["hp"])
-    updateFrameText()
+    updateStatsFrameText()
     
     if currentEnemy["hp"] <= 0:
         print("netopierik zomrel :((")
@@ -489,6 +496,8 @@ def updatePlayerHealth(health):
             
         playerHearts[i].config(image=newImage)
         playerHearts[i].image = newImage
+    
+    updateStatsFrameText()
 
 def drawPlayerHealth():
     global playerHearts, playerHeartImages
@@ -501,52 +510,53 @@ def drawPlayerHealth():
         }
         
         heart1Label = tk.Label(statsFrame, image=playerHeartImages['FULL'], bd=0, highlightthickness=0, relief='flat')
-        heart1Label.place(x=535, y=12)
+        heart1Label.place(x=540, y=12)
         playerHearts.append(heart1Label)
     
         heart2Label = tk.Label(statsFrame, image=playerHeartImages['FULL'], bd=0, highlightthickness=0, relief='flat')
-        heart2Label.place(x=535, y=68)
+        heart2Label.place(x=540, y=68)
         playerHearts.append(heart2Label)
         
         heart3Label = tk.Label(statsFrame, image=playerHeartImages['FULL'], bd=0, highlightthickness=0, relief='flat')
-        heart3Label.place(x=535, y=124)
+        heart3Label.place(x=540, y=124)
         playerHearts.append(heart3Label)
         
         heart4Label = tk.Label(statsFrame, image=playerHeartImages['FULL'], bd=0, highlightthickness=0, relief='flat')
-        heart4Label.place(x=535, y=180)
+        heart4Label.place(x=540, y=180)
         playerHearts.append(heart4Label)
         
         heart5Label = tk.Label(statsFrame, image=playerHeartImages['FULL'], bd=0, highlightthickness=0, relief='flat')
-        heart5Label.place(x=535, y=236)
+        heart5Label.place(x=540, y=236)
         playerHearts.append(heart5Label)
         
     except Exception:
         print("chyba pri nacitani obrazkov srdiecka (hrac) ")
 
 def statsFrameText():
-    global enemyHealthText
+    global enemyHealthText, playerHealthText
     currentEnemy = enemies[-1]
     enemyHealthText = tk.Label(
         statsFrame,
         text="Život: " + str(currentEnemy["hp"]),
-        font=('ARIAL', 30),
+        font=('ARIAL', 35),
         bg='#7F7F7F',
         fg='red'
         )
     playerHealthText = tk.Label(
         statsFrame, 
         text = ("Život: " + str(playerHealth)),
-        font = ('ARIAL', 30), 
+        font = ('ARIAL', 35), 
         bg = '#7F7F7F', 
         fg = 'cyan'
         )
     
-    enemyHealthText.place(relx=0.45, rely=0.1, anchor='ne')
-    playerHealthText.place(relx=0.55,  rely=0.1, anchor='nw')
+    enemyHealthText.place(x=295, rely=0.12, anchor='ne')
+    playerHealthText.place(x=310,  rely=0.12, anchor='nw')
     
-def updateFrameText():
+def updateStatsFrameText():
     currentEnemy = enemies[-1]
     enemyHealthText.config(text="Život: "+str(currentEnemy["hp"]))
+    playerHealthText.config(text="Život: " + str(playerHealth))
 
 def loadWordList():
     global wordList
@@ -568,7 +578,7 @@ def loadWordList():
         print("nenacitali sa slova")
         wordList = ['byt', 'strom', 'jaskyna', 'kuzlo', 'pismeno', 'hra', 'poklad', 'dungeon']
 
-def createGame():
+def createGame(level):
     global game, enemyCanvas,statsFrame, textFrame, lettersFrame, actionFrame
     
     game = tk.Tk()
@@ -635,7 +645,7 @@ def createGame():
     drawLetters()
     
     # enemiesFrame
-    createEnemies("bat")
+    createEnemies(level, "bat")
     
     # statsFrame texty
     statsFrameText()
